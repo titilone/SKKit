66a22df6d632d2ad77f0421e651ba8e202dabdf2
titilone <huijian0gao@126.com>
2025-02-13 08:58:28 +0800

    创建工程
    创建工程

---
+++ SKKitFramework/SKKitFramework/SKKitFramework.h
@@ -0,0 +1,18 @@
+//
+//  SKKitFramework.h
+//  SKKitFramework
+//
+//  Created by Admin on 2025/2/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SKKitFramework.
+FOUNDATION_EXPORT double SKKitFrameworkVersionNumber;
+
+//! Project version string for SKKitFramework.
+FOUNDATION_EXPORT const unsigned char SKKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKKitFramework/PublicHeader.h>
+
+
